c5fe8f674222531fd45f37d66cb642f09c4b202f
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-03-31 22:55:11 +0800

    Git commit final version before demo

--- script.js
+++ script.js
@@ -4,26 +4,33 @@ console.log("HELLO LET'S MAKE SOME FRUIT JUICE!");
 //Variables
 var instructions = document.querySelector('.instructions');
 var recipe = document.querySelector('.container-3');
-recipe.style.visibility = 'hidden';
 var ingredients = document.querySelector('.container-2');
-ingredients.style.visibility = 'hidden';
-var clickText;
-var gameStaging;
-var roundInSession = false;
-var ingredientsMatch = 0;
 var unsatisfiedCustomers = document.getElementById('unsatisfied-customer');
-var unsatisfiedCustomersCount = 0;
 var happyCustomers = document.getElementById('happy-customers');
+var countdownTimer = document.querySelector('.time');
+var recipeDisplay = document.querySelector('.recipe-display');
+var ingredientDisplay = document.querySelectorAll('.ingredient-display');
+
+recipe.style.visibility = 'hidden';
+ingredients.style.visibility = 'hidden';
+
+var idToRemove;
+var gameStaging;
+var unsatisfiedCustomersCount = 0;
 var happyCustomersCount = 0;
 var drinkIngredientCount = 0;
-var step = 0;
+var time = 60;
+var timer;
+var hasGameStarted = false;
+var displayDrinksOrder = [];
+var clickIngredients;
+var selectedIngredients = [];
+var drinkName = [];
+var drinkIngredients = [];
+var randomDrinkOption;
+var ingredientIndex = 0;
 
-
-
-
-
-
-//Game Variables
+//Drink object
 var drinkRecipe = [
     avoHoney = {
         name: 'Avocado Honey',
@@ -51,18 +58,6 @@ var drinkRecipe = [
     }
 ]
 
-
-var displayDrinksOrder = [];
-var clickIngredients;
-var selectedIngredients = [];
-var drinkName = [];
-var drinkIngredients = [];
-var randomDrinkOption;
-var drinkIngredientArray = [];
-
-
-
-
 for(var i = 0; i < drinkRecipe.length; i++) {
     drinkName.push(drinkRecipe[i].name);
 }
@@ -71,79 +66,129 @@ for(var i = 0; i < drinkRecipe.length; i++) {
     drinkIngredients.push(drinkRecipe[i].recipe);
 }
 
-/*
-if(randomDrinkOption === selectedIngredients) {
-        gameStaging.innerText = 'WELL DONE!';
+
+//Functions
+var removeIngredientClick = function(element) {
+    idToRemove = document.getElementById(element);
+    idToRemove.removeEventListener('click', selectIngredients);
+}
+
+var setTimer = function() {
+    timer = setInterval(updateCountdown, 1000);
+}
+
+var clearTimer = function() {
+    clearInterval(timer);
+}
+
+//Determines the outcome of the game based on the scoreboard's results.
+var scoreboardWinner = function() {
+    if(unsatisfiedCustomersCount === 3 || time === 0) {
+        gameStaging.innerText = 'YOU HAVE LOST THE GAME.';
+        clearTimer();
+        //removeIngredientClick(selectedIngredients[i]);
+    } else if (happyCustomersCount === 5 && time > 0) {
+        gameStaging.innerText = 'WELL DONE, YOU HAVE ATTAINED A BLACK BELT IN FRUIT JUICE MAKING!';
+        clearTimer();
+        //removeIngredientClick(selectedIngredients[i]);
     } else {
-        gameStaging.innerText = 'YOU HAVE TO DO BETTER!';
+        gameStaging.innerText = displayDrinksOrder;
     }
-    */
-var ingredientIndex = 0;
+}
 
+//Removes the strike through line once new random drink is generated.
+var removeLine = function () {
+    for(var i = 0; i < ingredientDisplay.length; i++) {
+        ingredientDisplay[i].style.textDecoration = 'none';
+    }
+}
+
+//Strike through of recipe ingredient once correct ingredient has been selected.
+var strikeThrough = function() {
+    for(var i = 0; i < selectedIngredients.length; i++){
+        for(var j = 0; j < ingredientDisplay.length; j++) {
+            if(selectedIngredients[i] === ingredientDisplay[j].innerText) {
+                    ingredientDisplay[j].style.textDecoration = "line-through";
+            }
+        }
+    }
+}
+
+//Countdown timer
+var updateCountdown = function(){
+    var seconds = time;
+    countdownTimer.innerText = seconds;
+    if(time <= 0) {
+    countdownTimer.innerText = 'Time is up! You\'re not quite there yet.';
+    countdownTimer.style.fontSize = '15px';
+    }
+  time--;
+}
+
+//Checks for match and whether player has won or lost the game based on the rules.
 var checkForMatch = function() {
-
     var drinkOrder = gameStaging.innerText;
-        console.log(drinkOrder + " LATEST DRINK ORDER!");
 
-        switch(drinkOrder) {
+    switch(drinkOrder) {
 
-          case drinkName[0]:
+        case drinkName[0]:
             randomDrinkOption = drinkIngredients[0];
             break;
 
-          case drinkName[1]:
+        case drinkName[1]:
             randomDrinkOption = drinkIngredients[1];
             break;
 
-          case drinkName[2]:
+        case drinkName[2]:
             randomDrinkOption = drinkIngredients[2];
             break;
 
-          case drinkName[3]:
+        case drinkName[3]:
             randomDrinkOption = drinkIngredients[3];
             break;
 
-          case drinkName[4]:
+        case drinkName[4]:
             randomDrinkOption = drinkIngredients[4];
             break;
 
-            default:
+        default:
             gameStaging.innerText = 'Sorry this drink is currently unavailable';
-        }
-
+    }
 
        for(var i = 0; i < selectedIngredients.length; i++) {
         if(randomDrinkOption.includes(selectedIngredients[i])) {
-            gameStaging.innerText = 'Correct ingredient, what is next?';
-
+            removeIngredientClick(selectedIngredients[i]);
             drinkIngredientCount++;
-            console.log(drinkIngredientCount + " COUNT");
 
             if((drinkIngredientCount/3) === 5) {
-                gameStaging.innerText = 'Correct order! Click for your next order';
                 selectedIngredients = [];
                 drinkIngredientCount = 0;
                 happyCustomersCount++;
                 happyCustomers.innerText = happyCustomersCount.toString();
-                gameStaging.addEventListener('click', randomGenerate);
+                removeLine();
+                randomGenerate();
             }
 
         } else if (selectedIngredients.length >= 5) {
-                gameStaging.innerText = 'Wrong order. Don\'t worry, we\'ll bounce back. Click here for your next order.';
-                selectedIngredients = [];
-                drinkIngredientCount = 0;
-                unsatisfiedCustomersCount++;
-                unsatisfiedCustomers.innerText = unsatisfiedCustomersCount.toString();
-                gameStaging.addEventListener('click', randomGenerate);
+            selectedIngredients = [];
+            drinkIngredientCount = 0;
+            unsatisfiedCustomersCount++;
+            unsatisfiedCustomers.innerText = unsatisfiedCustomersCount.toString();
+            removeLine();
+            randomGenerate();
         } else {
-                gameStaging.innerText = 'Wrong ingredient. You need to focus!';
+            removeIngredientClick(selectedIngredients[i]);
         }
-    }
-    }
+      }
+    strikeThrough();
+    scoreboardWinner();
+}
 
 
+
+
+//Created empty array to store selected ingredients using ingredients' id.
 var selectIngredients = function(event) {
-
     var ingredientsId = event.target.id;
     selectedIngredients.push(ingredientsId);
     instructions.innerText = '';
@@ -153,75 +198,68 @@ var selectIngredients = function(event) {
 //Function that randomly generates a drink order
 var generateDrinkOrder = function(array) {
 
-        var drinkIndex = Math.floor(Math.random() * 5);
-            array[drinkIndex];
-            displayDrinksOrder = array[drinkIndex].name;
-            console.log(displayDrinksOrder);
-            gameStaging.innerText = '';
-            gameStaging.innerText = displayDrinksOrder;
+    var drinkIndex = Math.floor(Math.random() * 5);
+    array[drinkIndex];
+    displayDrinksOrder = array[drinkIndex].name;
+    gameStaging.innerText = displayDrinksOrder;
+    gameStaging.style.fontSize = '25px';
+    gameStaging.style.cursor = 'pointer';
+    recipeDisplay.innerText = displayDrinksOrder;
 
-        //Add click event listeners to all ingredients
-            clickIngredients = document.querySelectorAll('.ingredients');
+//Displays recipe of random drink at container 4.
+    for(var i = 0; i < drinkRecipe.length; i++) {
+        if(displayDrinksOrder === drinkRecipe[i].name) {
+            ingredientDisplay[0].innerText = drinkRecipe[i].recipe[0];
+            ingredientDisplay[1].innerText = drinkRecipe[i].recipe[1];
+            ingredientDisplay[2].innerText = drinkRecipe[i].recipe[2];
+            ingredientDisplay[3].innerText = drinkRecipe[i].recipe[3];
+            ingredientDisplay[4].innerText = drinkRecipe[i].recipe[4];
 
-
-        for(var i = 0; i < clickIngredients.length; i++) {
-            clickIngredients[i].addEventListener('click', selectIngredients)
+            ingredientDisplay[0].style.fontSize = '30px';
+            ingredientDisplay[1].style.fontSize = '30px';
+            ingredientDisplay[2].style.fontSize = '30px';
+            ingredientDisplay[3].style.fontSize = '30px';
+            ingredientDisplay[4].style.fontSize = '30px';
         }
-
-
+    }
+//Add click event listeners to all ingredients
+clickIngredients = document.querySelectorAll('.ingredients');
+    for(var i = 0; i < clickIngredients.length; i++) {
+        clickIngredients[i].addEventListener('click', selectIngredients)
+    }
 }
 
-//
-var randomGenerate = function(event) {
-    console.log("Let's randomly generate you some orders!");
-
-        generateDrinkOrder(drinkRecipe);
+var randomGenerate = function() {
+    generateDrinkOrder(drinkRecipe);
+    if(!hasGameStarted) {
+        hasGameStarted = true;
+        setTimer();
     }
-
-
+}
 
 //Game layout orientation and lead-up to start of the game.
 var beginGame = function(event) {
-    console.log("LETS START THIS SHIT!");
-
-    event.target.innerText = "Here's a simple orientation of the game layout. This box will flash your orders as well as display your scoreboard as the game goes along. The ingredients list is to your right and you will need to refer to the recipe below this box to know which ingredients to select.";
     instructions.innerText = '';
-
-    event.target.style.fontSize = '25px';
     recipe.style.visibility = 'visible';
     ingredients.style.visibility = 'visible';
     document.querySelector('.container-1').style.height = '300px';
-    var startGame = document.createElement('p');
-    startGame.textContent = 'Click here to start and get your first set of orders!';
-    //var startRandomGenerator = document.querySelector('.random-generator');
-    gameStaging.innerText = '';
-    gameStaging.appendChild(startGame);
-    gameStaging.addEventListener('click', randomGenerate)
-
+    //gameStaging.innerText = '';
+    randomGenerate();
 }
 
 //Callback function that brings to player the instructions and next step that will see the game being launched.
 var startGame = function(event) {
-
     var beginGameStep = document.createElement('p');
-    beginGameStep.textContent = 'Click here to proceed';
+    beginGameStep.textContent = 'Click here to begin';
+    beginGameStep.style.fontSize = '25px';
     gameStaging = document.querySelector('.game-staging');
     gameStaging.appendChild(beginGameStep);
+    event.target.innerText = "Welcome to Fruit Express! Based on the random order displayed, click on the ingredients from the ingredients list to concoct the drink. Whichever ingredient you click will go straight into the blender and ultimately be consumed by the customer. You gain a happy customer for every successful cup of juice prepared and 1 unsatisfied customer for every wrong order prepared. Failure to complete 5 successful drinks will result in a loss. You also lose the game if you hit 3 unsatisfied customers. Good luck!";
+
     gameStaging.addEventListener('click', beginGame);
-
-    event.target.innerText = "Welcome to Fruits are King! The objective of this game is simple - keep your customers happy . \n\n  Based on the random order shown, click on the ingredients from the ingredients list to concoct the drink. Whichever ingredient you click will go straight into the blender and ultimately be consumed by the customer. Any wrong concoction will result in an unsatisfied customer and will see your unsatisfied customer count go up. You gain a commendation for every 5 successful cups of juice prepared and 1 unsatisfied customer for every wrong order prepared. You lose the game once you have 5 unsatisfied customers. Good luck!";
-
     document.querySelector('.container-1').style.height = '300px';
-    //document.querySelector('.container-1').style.padding = '0';
     instructions.style.fontSize = '16px';
-
 }
-
-
-
-
-
-
 
 //EVENT LISTENERS
 instructions.addEventListener('click', startGame);
